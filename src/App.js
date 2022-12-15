d8cb724ec7f27d7b0286bfb046fbdd147877d751
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-12-15 00:51:31 -0500

    finished the components for the post comments and updated like count

--- src/App.js
+++ src/App.js
@@ -32,6 +32,15 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const updatedLikes = posts.map(post => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes + 1 };
+      } else {
+        return post;
+      }
+    })
+
+    setPosts(updatedLikes);
   };
 
   return (
